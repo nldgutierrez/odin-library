5cfc44417ee2f4315901748c588d203d202bb314
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2025-06-04 13:26:13 +0800

    Display books after form submission
    Add row to table per item submitted in the form
    and display accordingly.

--- script.js
+++ script.js
@@ -18,16 +18,51 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     library.push(book);
 }
-// Save the user's input from the form
 
-// Get the user input values
+// DOM Selects
 const form = document.querySelector('#newBook');
+const table = document.querySelector('tbody');
+
+
+// Save user's input from the form
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
+
+    // Get the user input values
+
     const title = form.title.value;
     const author = form.author.value;
     const pages = form.pages.value;
     const read = form.read.checked;
+
     addBookToLibrary(title, author, pages, read);
+    
+    displayBook();
 });
+
+// Add Book to Table
+
+function displayBook() {
+    table.innerHTML = '';
+    for (let i = 0; i < library.length; i++) {
+        const row = document.createElement('tr');
+        const cellNum = document.createElement('td');
+        const cellTitle = document.createElement('td');
+        const cellAuthor = document.createElement('td');
+        const cellPages = document.createElement('td');
+        const cellRead = document.createElement('td');
+
+        cellNum.textContent = i+1;
+        cellTitle.textContent = library[i].title;
+        cellAuthor.textContent = library[i].author;
+        cellPages.textContent = library[i].pages;
+        cellRead.textContent = library[i].read;
+
+        table.appendChild(row).appendChild(cellNum);
+        table.appendChild(row).appendChild(cellTitle);
+        table.appendChild(row).appendChild(cellAuthor);
+        table.appendChild(row).appendChild(cellPages);
+        table.appendChild(row).appendChild(cellRead);
+    }
+}
